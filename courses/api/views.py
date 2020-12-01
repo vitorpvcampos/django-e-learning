7a4a85b500518e0fdc60dbca8a7f40f34759d575
uraeus <vitor.campos@engenharia.ufjf.br>
2020-12-01 16:57:08 -0300

    Created viewsets and routers close #44

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,10 +1,11 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
 from ..models import Subject, Course
-from .serializers import SubjectSerializer
+from .serializers import SubjectSerializer, CourseSerializer
 from django.shortcuts import get_object_or_404
 
 
@@ -18,11 +19,12 @@ class SubjectDetailView(generics.RetrieveAPIView):
     serializer_class = SubjectSerializer
 
 
-class CourseEnrollView(APIView):
-    authentication_classes = (BasicAuthentication,)
-    permission_classes = (IsAuthenticated,)
+class CourseViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
 
-    def post(self, request, pk, format=None):
-        course = get_object_or_404(Course, pk=pk)
+    @action(detail=True, methods=['post'], authentication_classes=[IsAuthenticated])
+    def enroll(self, request, *args, **kwargs):
+        course = self.get_object()
         course.students.add(request.user)
         return Response({'enrolled': True})
--- students/views.py
+++ students/views.py
@@ -1,12 +1,12 @@
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView, FormView
-from django.views.generic.list import ListView
-from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .forms import CourseEnrollForm
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from courses.models import Course
+from .forms import CourseEnrollForm
 
 
 class StudentRegistrationView(CreateView):
@@ -17,7 +17,8 @@ class StudentRegistrationView(CreateView):
     def form_valid(self, form):
         result = super().form_valid(form)
         cd = form.cleaned_data
-        user = authenticate(username=cd['username'], password=cd['password'])
+        user = authenticate(username=cd['username'],
+                            password=cd['password1'])
         login(self.request, user)
         return result
 
@@ -32,7 +33,8 @@ class StudentEnrollCourseView(LoginRequiredMixin, FormView):
         return super().form_valid(form)
 
     def get_success_url(self):
-        return reverse_lazy('student_course_detail', args=[self.course.id])
+        return reverse_lazy('student_course_detail',
+                            args=[self.course.id])
 
 
 class StudentCourseListView(LoginRequiredMixin, ListView):
@@ -54,9 +56,13 @@ class StudentCourseDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        # get course object
         course = self.get_object()
         if 'module_id' in self.kwargs:
-            context['module'] = course.modules.get(id=self.kwargs['module_id'])
+            # get current module
+            context['module'] = course.modules.get(
+                id=self.kwargs['module_id'])
         else:
+            # get first module
             context['module'] = course.modules.all()[0]
         return context
