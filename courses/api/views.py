712783a182732e8cbe521cd1aec33e4c83d4fe09
uraeus <vitor.campos@engenharia.ufjf.br>
2020-12-01 16:24:38 -0300

    Custom API views and authentication close #42

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,6 +1,11 @@
 from rest_framework import generics
-from ..models import Subject
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from ..models import Subject, Course
 from .serializers import SubjectSerializer
+from django.shortcuts import get_object_or_404
 
 
 class SubjectListView(generics.ListAPIView):
@@ -11,3 +16,13 @@ class SubjectListView(generics.ListAPIView):
 class SubjectDetailView(generics.RetrieveAPIView):
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
+
+
+class CourseEnrollView(APIView):
+    authentication_classes = (BasicAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request, pk, format=None):
+        course = get_object_or_404(Course, pk=pk)
+        course.students.add(request.user)
+        return Response({'enrolled': True})
